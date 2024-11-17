98ed93b161dae0a4157071193432ecf7bcbe1d57
Pharel Makona <pharelmakona@gmail.com>
2024-11-17 20:14:45 +0300

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,26 +2,55 @@
 
 const request = require('request');
 
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
-    }
-  });
-};
+// Get the movie ID from the command line arguments
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
 
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
+// Base URL for the Star Wars API
+const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
+
+// Fetch the movie data
+request(`${baseUrl}${movieId}/`, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
   }
-);
+
+  if (res.statusCode !== 200) {
+    console.error(`Error: ${res.statusCode} - ${res.statusMessage}`);
+    process.exit(1);
+  }
+
+  // Parse the movie data
+  const movieData = JSON.parse(body);
+  const characters = movieData.characters;
+
+  // Function to fetch and print character names in order
+  const fetchCharacterName = (url, callback) => {
+    request(url, (err, res, body) => {
+      if (err) return callback(err);
+      const characterData = JSON.parse(body);
+      callback(null, characterData.name);
+    });
+  };
+
+  // Iterate over characters and fetch their names
+  const characterPromises = characters.map(
+    (url) =>
+      new Promise((resolve, reject) => {
+        fetchCharacterName(url, (err, name) => {
+          if (err) reject(err);
+          else resolve(name);
+        });
+      })
+  );
+
+  // Wait for all character names and print them
+  Promise.all(characterPromises)
+    .then((names) => names.forEach((name) => console.log(name)))
+    .catch((err) => console.error(err));
+});
+

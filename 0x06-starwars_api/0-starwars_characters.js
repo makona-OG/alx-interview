76be3c0c36a923c27b3d1e1b56227f85fc115114
Pharel Makona <pharelmakona@gmail.com>
2024-11-21 23:14:20 +0300

    stars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,56 +1,25 @@
 #!/usr/bin/node
+/**
+  prints all characters of a Star Wars movie
+  */
 
+const myArgs = process.argv.slice(2);
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
 
-// Get the movie ID from the command line arguments
-const movieId = process.argv[2];
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
-
-// Base URL for the Star Wars API
-const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
-
-// Fetch the movie data
-request(`${baseUrl}${movieId}/`, (err, res, body) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-
-  if (res.statusCode !== 200) {
-    console.error(`Error: ${res.statusCode} - ${res.statusMessage}`);
-    process.exit(1);
-  }
-
-  // Parse the movie data
-  const movieData = JSON.parse(body);
-  const characters = movieData.characters;
-
-  // Function to fetch and print character names in order
-  const fetchCharacterName = (url, callback) => {
-    request(url, (err, res, body) => {
-      if (err) return callback(err);
-      const characterData = JSON.parse(body);
-      callback(null, characterData.name);
-    });
-  };
-
-  // Iterate over characters and fetch their names
-  const characterPromises = characters.map(
-    (url) =>
-      new Promise((resolve, reject) => {
-        fetchCharacterName(url, (err, name) => {
-          if (err) reject(err);
-          else resolve(name);
+request(url, async function (error, response, body) {
+  if (!error) {
+    const json = JSON.parse(body);
+    const endpoints = json.characters;
+    for (const endpoint of endpoints) {
+      await new Promise(function (resolve, reject) {
+        request(endpoint, function (error, response, body) {
+          if (!error) {
+            console.log(JSON.parse(body).name);
+            resolve();
+          }
         });
-      })
-  );
-
-  // Wait for all character names and print them
-  Promise.all(characterPromises)
-    .then((names) => names.forEach((name) => console.log(name)))
-    .catch((err) => console.error(err));
+      });
+    }
+  }
 });
-
